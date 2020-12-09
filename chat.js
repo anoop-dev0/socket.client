ec151d50adbef652ebe8a53428bf289e1eb4db5c
Anoop <anoop.living@gmail.com>
2020-12-09 19:35:51 +0530

    updated according to changes in socket-app

--- chat.js
+++ chat.js
@@ -1,98 +1,101 @@
-// Make connection
-const ec2_URL = "http://3.212.251.206:4000";
+
+const ec2_URL = "http://3.139.194.131:4000/";
 //const ec2_URL = "http://localhost:4000";
 let showChat = true;
+
 function getBuzzId() {
-    small = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    capital = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    buzzId = Math.ceil(Math.random() * 10000) + small[Math.floor(Math.random()*25)] + capital[Math.floor(Math.random()*25)];
-    alert(`Your Buzz-Id is ${buzzId}`);
-    console.log(`Your Buzz-Id is ${buzzId}`);
-    
-    return buzzId;
+    let person = prompt("Please enter your name", "Harry Potter");
+    if (person != null) {
+        return person
+    }
+    else
+        return getBuzzId()
 }
 var socket = io.connect(ec2_URL, {
-    query: { buzzId: getBuzzId() }
+    query: { buzzId: getBuzzId().toLowerCase() }
 });
 
 // Query DOM
 var message = document.getElementById('message'),
-    handle = document.getElementById('handle'),
     btn = document.getElementById('send'),
     output = document.getElementById('output'),
     feedback = document.getElementById('feedback'),
     btnGenGro = document.getElementById("addToGroup"),
     isGroup = document.getElementById("sendToGroup"),
     buzzIdStr = document.getElementById("buzzList"),
-recipient = document.getElementById('recipient');
+    groupNameHandler = document.getElementById("groupName"),
+    recipient = document.getElementById('recipient');
 
 // Emit events
-btnGenGro.addEventListener('click',function(){
-    if(buzzIdStr.value){
+message.addEventListener('keypress', function(){
+    socket.emit('typing', { recipient: recipient.value.toLowerCase() });
+})
+btnGenGro.addEventListener('click', function () {
+    if (buzzIdStr.value) {
         let buzzList = buzzIdStr.value.split(",");
-        socket.emit('create-group',{handle:handle.value,buzzList});
+        socket.emit('create-group', { groupName: groupNameHandler.value, buzzList });
     }
 })
 btn.addEventListener('click', function () {
-    console.log(isGroup.checked);
-    // if(recipient.value.startsWith("G") || recipient.value.startsWith("g")){
-    //     socket.emit('send-group', {
-    //         message: message.value,
-    //         handle: handle.value,
-    //         roomId : recipient.value
-    //     });
-    // }
-    // else
-     if (recipient.value) {
-         if(isGroup.checked)
-         {
-            socket.emit('send-group', {
-                message: message.value,
-                handle: handle.value,
-                recipient:recipient.value
-            });
-        }
-        else{
+    if (recipient.value) {
+        if (!isGroup.checked) {
             socket.emit('chat-private', {
                 message: message.value,
-                handle: handle.value,
-                recipient:recipient.value
+                recieverId: recipient.value.toLowerCase(),
+                isGroupMessage: isGroup.checked
+            });
+        }
+        else {
+            socket.emit('chat-group', {
+                message: message.value,
+                groupId: recipient.value.toLowerCase(),
             });
         }
     }
-    else{
-        socket.emit('chat-public', {
-            message: message.value,
-            handle: handle.value,
-            recipient:null
-        });
-    }
+
     message.value = "";
-    handle.disabled = true;
 });
 
-message.addEventListener('keypress', function () {
-    if (recipient.value) {
-        socket.emit('typing-private', handle.value);
-    }
-    else{
-        socket.emit('typing-public', handle.value);
-    }
-})
+// message.addEventListener('keypress', function () {
+//     if (recipient.value) {
+//         socket.emit('typing-private', handle.value);
+//     }
+//     else {
+//         socket.emit('typing-public', handle.value);
+//     }
+// })
 
 // Listen for events
 socket.on('chat', function (data) {
     feedback.innerHTML = '';
-    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+    output.innerHTML += '<p><strong>' + data.senderName + ': </strong>' + data.message + '</p>';
+});
+socket.on('chat-group', function (data) {
+    feedback.innerHTML = '';
+    output.innerHTML += '<p><strong>' + data.senderName + ' ('+ data.groupName +') : </strong>' + data.message + '</p>';
 });
 socket.on('recieve-msg', function (data) {
     feedback.innerHTML = '';
-    output.innerHTML += '<p><strong>' + data.sentBy + '</strong> ('+data.roomId+') : ' + data.message + '</p>';
+    output.innerHTML += '<p><strong>' + data.sentBy + '</strong> ( ' + data.roomId + ' ) : ' + data.message + '</p>';
 });
-socket.on('added-group',function (data) {
-    output.innerHTML += '<p>You have been added to group <strong> ' + data.roomId + ' </strong> by user <strong>' + data.addedBy + '</p>';
+socket.on('added-group', function (data) {
+    output.innerHTML += '<p>You have been added to group <strong> ' + data.groupName + '('+ data.groupId +') </strong> by <strong>' + data.addedBy + '</p>';
 })
-
+socket.on('group-created', function (data) {
+    if(data.success)
+        output.innerHTML += '<p><strong> ' + data.groupName + ' </strong> created by you with id <strong>' + data.groupId + '</p>';
+    else
+        alert("Group could not be created")
+})
 socket.on('typing', function (data) {
-    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+    feedback.innerHTML = '<p><em>' + data.typerName + ' is typing a message...</em></p>';
 });
+socket.on('online', function (data) {
+    alert(`${data.senderName} is online now`);
+});
+socket.on('offline', function (data) {
+    alert(`${data.senderName} is offline now`);
+});
+socket.on('unauthorized', function(data){
+    alert(data);
+})
